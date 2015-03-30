964f28325d4eee4c1370c1a6ed932bf9e867c00a
sashakmets <sashakmets@yandex.ru>
2015-03-30 03:27:01 +0200

    + product-manager

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 
 // (angularjs ajax json
@@ -13,20 +15,28 @@ var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 //   http://habrahabr.ru/post/120917/
 //   http://codehint.ru/articles/2013-06-13_asp_net_web_api_cors/2
 //   http://overcram.com/questions/?qid=411061
+// (AngularJS $routeProvider включением CORS)
+// * (Блог Makeomatic: разработка сайтов и мобильных приложений) https://makeomatic.ru/blog/2013/08/14/AngularJSIntro/
+//   https://gist.github.com/kkurni/4618210
+// * (AngularJS and CORS) http://draptik.github.io/blog/2013/08/19/angularjs-and-cors/
+//                        https://github.com/draptik/angulardemorestful
+//   http://georgi-naumov.blogspot.com/2014/02/angularjs-http-cors-and-http.html
 
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider
+app.config(['$routeProvider', '$httpProvider',
+    function($routeProvider, $httpProvider) {
+        $routeProvider
         .when('/', {
                 title: 'Products',
                 templateUrl: 'partials/products.tpl',
                 controller: 'productsCtrl'
             })
-            .otherwise({
-                redirectTo: '/'
+        .otherwise({
+            redirectTo: '/'
             });
-        //.defaults.headers.get["Content-Type"] = "application/x-www-form-urlencoded";
-}]);
+        $httpProvider.defaults.withCredentials = true;
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+        //$httpProvider.defaults.headers.get["Content-Type"] = "application/x-www-form-urlencoded";
+    }]);
 //.config(['$httpProvider',
 //    function ($httpProvider) {
 //    //$httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
