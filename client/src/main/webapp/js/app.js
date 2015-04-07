f72ffbb30ee3dbe996f82b48eb27aacea7d18615
dn200978lak <dn200978lak@gmail.com>
2015-04-07 16:18:22 +0300

    Добавляю общее меню в проект + новый стиль

--- client/src/main/webapp/js/app.js
+++ client/src/main/webapp/js/app.js
@@ -2,10 +2,10 @@
 
 /* App Module */
 
-angular.module('app', ['ngRoute', 'ui.bootstrap'])
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 
-.config(['$routeProvider',
-	function($routeProvider) {
+app.config(['$routeProvider', '$httpProvider',
+	function($routeProvider, $httpProvider) {
 		$routeProvider
             .when('/home', {
 				templateUrl: 'partials/home.html',
@@ -15,8 +15,14 @@ angular.module('app', ['ngRoute', 'ui.bootstrap'])
                 templateUrl: 'partials/customer.html',
                 controller: 'customersCrtl'
             })
+            .when('/products', {
+                templateUrl: 'partials/products.tpl',
+                controller: 'productsCtrl'
+            })
             .otherwise({
 				redirectTo: '/customer'
 			});
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
 	}
 ]);
