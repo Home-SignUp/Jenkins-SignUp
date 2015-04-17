022cf3d10450e4619c9c513133767688500b3a12
dn200978lak <dn200978lak@gmail.com>
2015-04-17 16:40:50 +0300

    Начинаю делать разделение бекенда на два модуля ('frontent'+'backend')

---
+++ frontend/src/main/java/com/addrbook/bootstrap/RootConfig.java
@@ -0,0 +1,17 @@
+package com.addrbook.bootstrap;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+* Bootstrap for service layer.
+*
+* @author Trey
+*/
+@Configuration
+@Import(DatabaseConfig.class)
+@ComponentScan(basePackages = {"com.addrbook.service", "com.addrbook.dao", "com.addrbook.util"})
+public class RootConfig {
+
+}
---
+++ frontend/src/main/java/com/addrbook/dao/PersonDao.java
@@ -0,0 +1,48 @@
+package com.addrbook.dao;
+
+import com.addrbook.domain.Customer;
+import com.addrbook.domain.Person;
+import com.addrbook.domain.Product;
+
+import java.util.List;
+
+/**
+ * DAO for person database table.
+ * 
+ * @author Trey
+ */
+public interface PersonDao {
+
+	/**
+	 * Selects the person record with the given id.
+	 * @param id
+	 * @return Returns the Person for the given id. Returns null if none found.
+	 */
+	Person findById(Integer id);
+
+    List<Person> getAllPersons();
+
+    public Customer getCustomer();
+
+    public List<Customer> getAllCustomers();
+
+    Product findProductById(Integer id);
+
+    public List<Product> getAllProducts();
+
+    void update(Product p);
+
+	/**
+	 * Inserts a person record using the non-id properties.
+	 * The id property is populated with the generated id.
+	 * @param person 
+	 */
+	void insert(Person person);
+
+	/**
+	 * Updates the person record with p.getId().
+	 * @param p
+	 */
+	void update(Person p);
+
+}
