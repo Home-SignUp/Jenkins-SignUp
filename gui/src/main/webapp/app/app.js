6be959c4259a5be1d33ac85403c05d777f72d0d2
sashakmets <sashakmets@yandex.ru>
2015-04-19 16:44:07 +0200

    Совмещаю таблицу 'Users' (P_roducts) с таблицей 'Customer'

--- gui/src/main/webapp/app/app.js
+++ gui/src/main/webapp/app/app.js
@@ -7,13 +7,9 @@ var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 app.config(['$routeProvider', '$httpProvider',
 	function($routeProvider, $httpProvider) {
 		$routeProvider
-            .when('/home', {
-				templateUrl: 'partials/home.tpl',
-				controller: 'HomeCtrl'
-			})
-            .when('/customer', {
-                templateUrl: 'partials/customer.tpl',
-                controller: 'CustomersCrtl'
+            .when('/users', {
+                templateUrl: 'partials/users.tpl',
+                controller: 'UsersCtrl'
             })
             .when('/p_roducts', {
                 templateUrl: 'partials/p_roducts.tpl',
@@ -23,8 +19,16 @@ app.config(['$routeProvider', '$httpProvider',
                 templateUrl: 'partials/products.tpl',
                 controller: 'ProductsCtrl'
             })
+            .when('/customer', {
+                templateUrl: 'partials/customer.tpl',
+                controller: 'CustomersCrtl'
+            })
+            .when('/home', {
+                templateUrl: 'partials/home.tpl',
+                controller: 'HomeCtrl'
+            })
             .otherwise({
-				redirectTo: '/products'
+				redirectTo: '/users'
 			});
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
