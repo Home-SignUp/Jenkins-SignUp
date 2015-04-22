62d3ac3de8aa6caed372a2d9c448c3ba0ab1072e
dn200978lak <dn200978lak@gmail.com>
2015-04-22 10:14:33 +0300

    Создал отдельный интерфейс (GUI) для базы 'products'! Начинаю создавать отдельный интерфейс (GUI) для базы 'users'.

--- gui/src/main/webapp/app/app.js
+++ gui/src/main/webapp/app/app.js
@@ -7,20 +7,28 @@ var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
 app.config(['$routeProvider', '$httpProvider',
 	function($routeProvider, $httpProvider) {
 		$routeProvider
-            .when('/register', {
-                templateUrl: 'partials/register.tpl',
-                controller: 'RegisterCtrl'
+            //.when('/register', {
+            //    templateUrl: 'partials/register.tpl',
+            //    controller: 'RegisterCtrl'
+            //})
+            //.when('/users', {
+            //    templateUrl: 'partials/users.tpl',
+            //    controller: 'UsersCtrl'
+            //})
+            .when('/register-product', {
+                templateUrl: 'partials/registerProduct.tpl',
+                controller: 'RegisterProductCtrl'
             })
-            .when('/users', {
-                templateUrl: 'partials/users.tpl',
-                controller: 'UsersCtrl'
+            .when('/products', {
+                templateUrl: 'partials/products.tpl',
+                controller: 'ProductsCtrl'
             })
             .when('/signup', {
                 templateUrl: 'partials/signup.tpl',
                 controller: 'SignupCtrl'
             })
             .otherwise({
-				redirectTo: '/register'
+				redirectTo: '/register-product'
 			});
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
