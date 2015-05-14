0ae18e6f857803518f4f30f50ba6a6f208e512c7
dn200978lak <dn200978lak@gmail.com>
2015-05-14 19:36:53 +0300

    Добавляю механизм авторизации/регистрации: http://jasonwatmore.com/post/2015/03/10/AngularJS-User-Registration-and-Login-Example.aspx http://javarelated.com/notes-on-angularjs-integration-with-jersey-part-1.html https://github.com/cornflourblue/angular-registration-login-example + http://uaihebert.com/user-authentication-filterservlet/ + http://www.slideshare.net/joshs633/angularjs-authentication-patterns http://www.codeitive.com/0zmWjgXXXW/angularjs-role-based-navigation.html http://stackoverflow.com/questions/26224079/angularjs-role-based-navigation http://stackoverflow.com/questions/21560555/angularjs-role-based-navigation http://www.frederiknakstad.com/2013/01/21/authentication-in-single-page-applications-with-angular-js/

--- gui/src/main/webapp/app/app.js
+++ gui/src/main/webapp/app/app.js
@@ -2,27 +2,67 @@
 
 /* App Module */
 
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngCookies'])
 
 app.config(['$routeProvider', '$httpProvider',
 	function($routeProvider, $httpProvider) {
 		$routeProvider
+            .when('/', {
+                controller: 'UsersCtrl',
+                templateUrl: 'partials/users.tpl'
+            })
+
             .when('/register', {
                 templateUrl: 'partials/register.tpl',
                 controller: 'RegisterCtrl'
             })
-            .when('/users', {
-                templateUrl: 'partials/users.tpl',
-                controller: 'UsersCtrl'
-            })
+            //.when('/users', {
+            //    templateUrl: 'partials/users.tpl',
+            //    controller: 'UsersCtrl'
+            //})
             .when('/signup', {
                 templateUrl: 'partials/signup.tpl',
                 controller: 'SignupCtrl'
             })
+
+            .when('/login2', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+            .when('/register2', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
+            .when('/home', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
+
             .otherwise({
-				redirectTo: '/register'
+				redirectTo: '/login2'
 			});
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
 	}
 ]);
+
+app.run = ['$rootScope', '$location', '$cookieStore', '$http'];
+function run($rootScope, $location, $cookieStore, $http) {
+    // keep user logged in after page refresh
+    $rootScope.globals = $cookieStore.get('globals') || {};
+    if ($rootScope.globals.currentUser) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        // redirect to login page if not logged in and trying to access a restricted page
+        var restrictedPage = $.inArray($location.path(), ['/login2', '/register2']) === -1;
+        var loggedIn = $rootScope.globals.currentUser;
+        if (restrictedPage && !loggedIn) {
+            $location.path('/login2');
+        }
+    });
+}
