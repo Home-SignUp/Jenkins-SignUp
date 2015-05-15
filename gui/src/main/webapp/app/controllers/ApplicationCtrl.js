ca4554b3f22b1e9b48065f6e317ffabd56bc1702
dn200978lak <dn200978lak@gmail.com>
2015-05-15 15:13:07 +0300

    На клиентской стороне поправил GUI-интерфейс для авторизации / регистрации
    http://stackoverflow.com/questions/26224079/angularjs-role-based-navigation
    http://stackoverflow.com/questions/21560555/angularjs-role-based-navigation

--- gui/src/main/webapp/app/controllers/ApplicationCtrl.js
+++ gui/src/main/webapp/app/controllers/ApplicationCtrl.js
@@ -2,9 +2,60 @@
 
 /* ApplicationCtrl */
 
-angular.module('app')
+//angular.module('app')
+angular
+    .module('app')
+    .controller('ApplicationCtrl', ApplicationCtrl);
 
-.controller('ApplicationCtrl', function($scope, $location) {
+//.controller('ApplicationCtrl', function($scope, $location, $UserService) {
+ApplicationCtrl.$inject = ['UserService', '$rootScope', '$scope', '$location'];
+function ApplicationCtrl(UserService, $rootScope, $scope, $location) {
+    var vm = this;
+
+    vm.user = null;
+    vm.allUsers = [];
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        var loggedIn = $rootScope.globals.currentUser;
+        if (loggedIn) {
+            initController();
+        }
+    });
+
+    $scope.isLogin = function() {
+        if(vm.user == null){
+            return false; //Just as a test to make sure it works
+        } else {
+            return true;
+        }
+    }
+    $scope.isLogout = function() {
+        if(vm.user == null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //$scope.IsAdmin = function(){
+    //    return $scope.UserRole == "Admin"; //Just as a test to make sure it works
+    //}
+    //$scope.IsUser = function(){
+    //    return $scope.UserRole == "StandardUser";
+    //}
+    //$scope.IsManager = function(){
+    //    return $scope.UserRole == "Manager";
+    //}
+
+    function initController() {
+        loadCurrentUser();
+    }
+    function loadCurrentUser() {
+        UserService.GetByUsername($rootScope.globals.currentUser.username)
+            .then(function (user) {
+                vm.user = user;
+            });
+    }
+
     $scope.isActive = function(path) {
         if ($location.path().substr(0, path.length) === path) {
             if (path === "/" && $location.path() === "/") {
@@ -18,4 +69,5 @@ angular.module('app')
             return false;
         }
     };
-})
+//})
+}
