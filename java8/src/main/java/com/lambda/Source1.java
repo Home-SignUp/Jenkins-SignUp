3fc4496c2af6422983667dfec3643be83109fb5e
dn200978lak <dn200978lak@gmail.com>
2015-05-19 20:00:26 +0300

    Перевел приложение на Java-8 - приложение собирается но серверная сторона недеплоиться?

--- java8/src/main/java/com/lambda/Source1.java
+++ java8/src/main/java/com/lambda/Source1.java
@@ -2,6 +2,7 @@ package com.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * @author Aleksandr Konstantinovitch
@@ -19,14 +20,57 @@ import java.util.List;
 public class Source1 {
 
     public static void main(String[] args) {
+        // Лямбда-выражение представляет собой блок кода, который можно передать в другое место, поэтому он может быть выполнен позже, один или несколько раз.
         // Лямбда-выражения в Java 8 — Простые примеры того, как они могут облегчить вашу жизнь
         // ( http://habrahabr.ru/post/213805/ )
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         for (int number : numbers) {
-            System.out.println(number);
+            System.out.print(number + " ");
         }
 
-        numbers.forEach((Integer value) -> System.out.println(value));
+        System.out.println();
+        numbers.forEach((Integer value) -> System.out.print(value + " "));
+
+        System.out.println();
+        numbers.forEach(System.out::print);
+
+
+        System.out.println();
+        // Копипаст или абстракции? Выбирать вам!
+        System.out.println("-----------------------------");
+        System.out.println(sumAll(numbers));
+        System.out.println(sumAllEven(numbers));
+
+//        sumAll(numbers, n -> true);
+//        sumAll(numbers, n -> n % 2 == 0);
+//        sumAll(numbers, n -> n > 3);
     }
 
+    public static int sumAll(List<Integer> numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        return total;
+    }
+
+    public static int sumAllEven(List<Integer> numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                total += number;
+            }
+        }
+        return total;
+    }
+
+    public int sumAll(List<Integer> numbers, Predicate<Integer> p) {
+        int total = 0;
+        for (int number : numbers) {
+            if (p.test(number)) {
+                total += number;
+            }
+        }
+        return total;
+    }
 }
